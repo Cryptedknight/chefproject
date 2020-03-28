777af4e13eb87f54b8b78ec37e838dd424a242ae
Utkarsh Dwivedi <udiisc100@gmail.com>
2020-03-28 20:39:04 +0530

    Updates

---
+++ precache-manifest.5cc3e1acedee4e32d8a68113ec41526e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2395ad37c95bc21be8bdf7391bffac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92a8faee6d5f7b0f1d2",
+    "url": "/static/css/main.283a766f.chunk.css"
+  },
+  {
+    "revision": "e6df06b88a797b95b4c6",
+    "url": "/static/js/2.a5736c0d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a5736c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92a8faee6d5f7b0f1d2",
+    "url": "/static/js/main.8300e1a9.chunk.js"
+  },
+  {
+    "revision": "1da24d7ff8b304797417",
+    "url": "/static/js/runtime-main.eab05a5c.js"
+  }
+]);
