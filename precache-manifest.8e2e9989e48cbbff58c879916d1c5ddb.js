9c1fa79d6139ede4c16697f819e45534cb179eff
Utkarsh Dwivedi <udiisc100@gmail.com>
2020-03-29 11:07:12 +0530

    Updates

---
+++ precache-manifest.8e2e9989e48cbbff58c879916d1c5ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7f6e212e710ab797e02874aba15fc7",
+    "url": "/codechef-app/index.html"
+  },
+  {
+    "revision": "df80b9a3e14dab721b5f",
+    "url": "/codechef-app/static/css/main.988e2ff3.chunk.css"
+  },
+  {
+    "revision": "da6b10b312ab276a8621",
+    "url": "/codechef-app/static/js/2.c8d90cac.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/codechef-app/static/js/2.c8d90cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df80b9a3e14dab721b5f",
+    "url": "/codechef-app/static/js/main.da8d1ad9.chunk.js"
+  },
+  {
+    "revision": "db81ffa040b634b028bb",
+    "url": "/codechef-app/static/js/runtime-main.0aaffd6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cc3e1acedee4e32d8a68113ec41526e.js"
+  "/codechef-app/precache-manifest.8e2e9989e48cbbff58c879916d1c5ddb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codechef-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
